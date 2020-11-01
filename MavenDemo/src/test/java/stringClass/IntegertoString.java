294028246a2359eb92797e271c3ce487dac822d4
maddy4455 <maddyrav.qa@gmail.com>
2020-11-01 11:43:26 -0700

    StringClass

---
+++ MavenDemo/src/test/java/stringClass/IntegertoString.java
@@ -0,0 +1,13 @@
+package stringClass;
+
+public class IntegertoString {
+
+	public static void main(String[] args) {
+		
+		int i=2021;
+		String s=Integer.toString(i);
+		System.out.println(s);
+		
+	}
+
+}
